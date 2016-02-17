2f57431b3033a1fed807c31235bd04f96387e0a9
Vien Van <vienvonvan@gmail.com>
2016-02-17 15:31:46 -0800

    Added images and show route

--- RockNRails/app/controllers/records_controller.rb
+++ RockNRails/app/controllers/records_controller.rb
@@ -1,11 +1,14 @@
 class RecordsController < ApplicationController
   def index
+    @records = Record.all
   end
 
   def show
+    @record = Record.find(params[:id])
   end
 
   def new
+    @record = Record.new
   end
 
   def create
